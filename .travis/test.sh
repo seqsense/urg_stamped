f87d68acf36d26c8f5c3e3f798c4783e176bdad0
Atsushi Watanabe <atsushi.w@ieee.org>
2018-08-22 16:08:21 +0900

    Add build matrix for ROS indigo/kinetic/melodic (#38)
    * Add build matrix
    
    * Fix workspace init
    
    * Fix rosdep argument
    
    * Fold test details
    
    * Fix test for latest g++

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 set -o errexit
-set -o verbose
 
 source /opt/ros/${ROS_DISTRO}/setup.bash
 
+set -o verbose
+
+cd /catkin_ws/src && catkin_init_workspace
 cd /catkin_ws
 
-apt-get -qq update && \
-apt-get install libxml2-utils && \
-rosdep install --from-paths src/${PACKAGE_NAME} --skip-keys=joystick_interrupt \
-  --ignore-src --rosdistro=${ROS_DISTRO} -y && \
-apt-get clean && \
+apt-get -qq update
+apt-get install libxml2-utils
+rosdep install --from-paths src/${PACKAGE_NAME} --ignore-src --rosdistro=${ROS_DISTRO} -y
+apt-get clean
 rm -rf /var/lib/apt/lists/*
 
 sed -i -e '5a set(CMAKE_C_FLAGS "-Wall -Werror")' \
@@ -23,7 +24,7 @@ CM_OPTIONS=""
 LOG=/tmp/catkin_make.log
 function error_log() {
   grep -A5 error ${LOG} > ${LOG}.error
-  echo -e '\n```'
+  echo -e '\n\n```'
   head -n30 ${LOG}.error
   ERR_LINES=`cat ${LOG}.error | wc -l`
   if [ $ERR_LINES -gt 30 ]
@@ -31,30 +32,31 @@ function error_log() {
     echo '--'
     echo "error log exceeded 30 lines (total $ERR_LINES lines)"
   fi
-  echo -e '```\n'
+  echo -e '```\n '
 }
 
 pip install gh-pr-comment
-FAILED_MESSAGE="FAILED on ROS ${ROS_DISTRO}"
-PASSED_MESSAGE="PASSED on ROS ${ROS_DISTRO}"
+FAILED_MESSAGE="[#${TRAVIS_BUILD_NUMBER}] FAILED on ROS ${ROS_DISTRO}"
+PASSED_MESSAGE="[#${TRAVIS_BUILD_NUMBER}] PASSED on ROS ${ROS_DISTRO}"
 
 (set -o pipefail; catkin_make ${CM_OPTIONS} 2>&1 | tee ${LOG}) \
   || (gh-pr-comment "${FAILED_MESSAGE}" \
-      "\`catkin_make\` failed$(error_log)"; false)
+      "<details><summary>catkin_make failed</summary>$(error_log)</details>"; false)
 (set -o pipefail; catkin_make install ${CM_OPTIONS} 2>&1 | tee ${LOG}) \
   || (gh-pr-comment "${FAILED_MESSAGE}" \
-      "\`catkin_make install\` failed$(error_log)"; false)
+      "<details><summary>catkin_make install failed</summary>$(error_log)</details>"; false)
 
 source /catkin_ws/devel/setup.bash
 
 (set -o pipefail; catkin_make tests ${CM_OPTIONS} 2>&1 | tee ${LOG}) \
   || (gh-pr-comment "${FAILED_MESSAGE}" \
-      "\`catkin_make tests\` failed$(error_log)"; false)
+      "<details><summary>catkin_make tests failed</summary>$(error_log)</details>"; false)
 (set -o pipefail; catkin_make run_tests ${CM_OPTIONS} 2>&1 | tee ${LOG}) \
   || (gh-pr-comment "${FAILED_MESSAGE}" \
-      "\`catkin_make run_tests\` failed$(error_log)"; false)
+      "<details><summary>catkin_make run_tests failed</summary>$(error_log)</details>"; false)
 
 result_text="
+
 \`\`\`
 `catkin_test_results --all | grep -v Skipping || true`
 \`\`\`
@@ -63,10 +65,7 @@ errored_tests=`catkin_test_results --all | grep -v -e "^Skipping" -v -e "^Summar
 result_text_detail="
 `echo ${errored_tests} | xargs -n 1 -- bash -c 'echo; echo \#\#\# $0; echo; echo \\\`\\\`\\\`xml; xmllint --format $0; echo \\\`\\\`\\\`;'`
 "
-catkin_test_results || (gh-pr-comment "${FAILED_MESSAGE}" "Test failed$result_text$result_text_detail"; false)
+catkin_test_results \
+  || (gh-pr-comment "${FAILED_MESSAGE}" "<details><summary>Test failed</summary>$result_text$result_text_detail</details>"; false)
 
-gh-pr-comment "${PASSED_MESSAGE}" "All tests passed$result_text"
-
-
-cd ..
-rm -rf /catkin_ws || true
+gh-pr-comment "${PASSED_MESSAGE}" "<details><summary>All tests passed</summary>$result_text</details>"
