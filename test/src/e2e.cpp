89b4666052e1218523fcf6a8e9902f62f54af347
Atsushi Watanabe <atsushi.w@ieee.org>
2024-03-05 16:19:05 +0900

    Update test

--- test/src/e2e.cpp
+++ test/src/e2e.cpp
@@ -20,6 +20,7 @@
 #include <boost/asio/ip/tcp.hpp>
 
 #include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
 
 #include <urg_stamped/urg_stamped.h>
 #include <urg_sim/urg_sim.h>
@@ -30,7 +31,9 @@ class E2E : public ::testing::Test
 {
 public:
   E2E()
-    : pnh_("~")
+    : nh_()
+    , pnh_("~")
+    , sub_cloud_(nh_.subscribe("cloud", 1, &E2E::cbCloud, this))
   {
   }
 
@@ -54,14 +57,25 @@ public:
         boost::asio::ip::tcp::endpoint(
             boost::asio::ip::tcp::v4(),
             0),
-        params);
+        params,
+        std::bind(&E2E::cbRawScanData, this, std::placeholders::_1));
     th_sim_ = std::thread(std::bind(&urg_sim::URGSimulator::spin, sim_));
 
     pnh_.setParam("ip_address", "127.0.0.1");
-    pnh_.setParam("ip_port", std::to_string(sim_->getLocalEndpoint().port()));
+    pnh_.setParam("ip_port", sim_->getLocalEndpoint().port());
 
     node_ = new urg_stamped::UrgStampedNode();
     th_node_ = std::thread(std::bind(&urg_stamped::UrgStampedNode::spin, node_));
+
+    const ros::Time deadline = ros::Time::now() + ros::Duration(5);
+    ros::Rate wait(1);
+    while (clouds_.size() == 0)
+    {
+      wait.sleep();
+      ros::spinOnce();
+      ASSERT_TRUE(ros::ok());
+      ASSERT_LT(ros::Time::now(), deadline) << "Timeout";
+    }
   }
 
   void TearDown()
@@ -72,11 +86,29 @@ public:
   }
 
 private:
+  ros::NodeHandle nh_;
   ros::NodeHandle pnh_;
+  ros::Subscriber sub_cloud_;
+
   urg_sim::URGSimulator* sim_;
   urg_stamped::UrgStampedNode* node_;
   std::thread th_sim_;
   std::thread th_node_;
+
+  std::vector<sensor_msgs::PointCloud2::ConstPtr> clouds_;
+  std::vector<urg_sim::RawScanData::Ptr> raw_scans_;
+
+  void cbCloud(const sensor_msgs::PointCloud2::ConstPtr& msg)
+  {
+    std::cerr << "cbCloud" << std::endl;
+    clouds_.push_back(msg);
+  }
+
+  void cbRawScanData(const urg_sim::RawScanData::Ptr data)
+  {
+    std::cerr << "cbRawScanData" << std::endl;
+    raw_scans_.push_back(data);
+  }
 };
 
 TEST_F(E2E, Dummy)
