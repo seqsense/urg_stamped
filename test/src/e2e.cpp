589b1b22c55c7ab08010a0d03a8edca979dd8bd2
Atsushi Watanabe <atsushi.w@ieee.org>
2024-03-06 14:54:58 +0900

    Add simple unit test

--- test/src/e2e.cpp
+++ test/src/e2e.cpp
@@ -33,27 +33,64 @@ public:
   E2E()
     : nh_()
     , pnh_("~")
-    , sub_scan_(nh_.subscribe("scan", 10, &E2E::cbScan, this))
+    , sub_scan_(nh_.subscribe("scan", 100, &E2E::cbScan, this))
     , cnt_(0)
   {
   }
 
-  void SetUp()
+  void TearDown()
   {
-    const urg_sim::URGSimulator::Params params =
-        {
-            .model = urg_sim::URGSimulator::Model::UTM,
-            .boot_duration = 0.05,
-            .comm_delay_base = 0.001,
-            .comm_delay_sigma = 0.0002,
-            .scan_interval = 0.025,
-            .clock_rate = 1.0,
-            .hex_ii_timestamp = false,
-            .angle_resolution = 1440,
-            .angle_min = 0,
-            .angle_max = 1080,
-            .angle_front = 540,
-        };
+    if (sim_)
+    {
+      sim_->kill();
+      th_sim_.join();
+      th_node_.join();
+    }
+  }
+
+private:
+  ros::NodeHandle nh_;
+  ros::NodeHandle pnh_;
+  ros::Subscriber sub_scan_;
+
+  urg_sim::URGSimulator* sim_;
+  urg_stamped::UrgStampedNode* node_;
+  std::thread th_sim_;
+  std::thread th_node_;
+
+  size_t cnt_;
+
+  void cbScan(const sensor_msgs::LaserScan::ConstPtr& msg)
+  {
+    scans_.push_back(msg);
+  }
+
+  void cbRawScanData(const urg_sim::RawScanData::Ptr data)
+  {
+    data->ranges[0] = cnt_;
+    cnt_++;
+    raw_scans_.push_back(data);
+  }
+
+protected:
+  std::vector<sensor_msgs::LaserScan::ConstPtr> scans_;
+  std::vector<urg_sim::RawScanData::Ptr> raw_scans_;
+
+  void waitScans(const size_t num, const ros::Duration& timeout)
+  {
+    const ros::Time deadline = ros::Time::now() + timeout;
+    ros::Rate wait(10);
+    while (scans_.size() <= num)
+    {
+      wait.sleep();
+      ros::spinOnce();
+      ASSERT_TRUE(ros::ok());
+      ASSERT_LT(ros::Time::now(), deadline) << "Timeout";
+    }
+  }
+
+  void startSimulator(const urg_sim::URGSimulator::Params& params)
+  {
     sim_ = new urg_sim::URGSimulator(
         boost::asio::ip::tcp::endpoint(
             boost::asio::ip::tcp::v4(),
@@ -69,54 +106,48 @@ public:
     node_ = new urg_stamped::UrgStampedNode();
     th_node_ = std::thread(std::bind(&urg_stamped::UrgStampedNode::spin, node_));
 
-    const ros::Time deadline = ros::Time::now() + ros::Duration(5);
-    ros::Rate wait(1);
-    while (scans_.size() == 0)
-    {
-      wait.sleep();
-      ros::spinOnce();
-      ASSERT_TRUE(ros::ok());
-      ASSERT_LT(ros::Time::now(), deadline) << "Timeout";
-    }
-  }
-
-  void TearDown()
-  {
-    sim_->kill();
-    th_sim_.join();
-    th_node_.join();
-  }
-
-private:
-  ros::NodeHandle nh_;
-  ros::NodeHandle pnh_;
-  ros::Subscriber sub_scan_;
-
-  urg_sim::URGSimulator* sim_;
-  urg_stamped::UrgStampedNode* node_;
-  std::thread th_sim_;
-  std::thread th_node_;
-
-  std::vector<sensor_msgs::LaserScan::ConstPtr> scans_;
-  std::vector<urg_sim::RawScanData::Ptr> raw_scans_;
-  size_t cnt_;
-
-  void cbScan(const sensor_msgs::LaserScan::ConstPtr& msg)
-  {
-    scans_.push_back(msg);
-  }
-
-  void cbRawScanData(const urg_sim::RawScanData::Ptr data)
-  {
-    data->ranges[0] = cnt_;
-    cnt_++;
-    raw_scans_.push_back(data);
+    waitScans(1, ros::Duration(5));
   }
 };
 
-TEST_F(E2E, Dummy)
+TEST_F(E2E, Simple)
 {
-  ASSERT_TRUE(true);
+  const urg_sim::URGSimulator::Params params =
+      {
+          .model = urg_sim::URGSimulator::Model::UTM,
+          .boot_duration = 0.01,
+          .comm_delay_base = 0.0001,
+          .comm_delay_sigma = 0.00001,
+          .scan_interval = 0.025,
+          .clock_rate = 1.0,
+          .hex_ii_timestamp = false,
+          .angle_resolution = 1440,
+          .angle_min = 0,
+          .angle_max = 1080,
+          .angle_front = 540,
+      };
+  startSimulator(params);
+
+  waitScans(100, ros::Duration(10));
+
+  std::map<int, ros::Time> expected_stamps_;
+  for (const urg_sim::RawScanData::Ptr raw : raw_scans_)
+  {
+    const int index = raw->ranges[0];
+    expected_stamps_[index] = ros::Time::fromBoost(raw->full_time);
+  }
+
+  int cnt = 0;
+  for (const sensor_msgs::LaserScan::ConstPtr scan : scans_)
+  {
+    cnt++;
+    if (cnt > 50)
+    {
+      const int index = std::lround(scan->ranges[0] * 1000);
+      ASSERT_NE(expected_stamps_.find(index), expected_stamps_.end()) << "Can not find corresponding ground truth timestamp";
+      ASSERT_LT(expected_stamps_[index] - scan->header.stamp, ros::Duration(0.0015));
+    }
+  }
 }
 
 int main(int argc, char** argv)
