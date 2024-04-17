b2d13657751e2ebc78cc5b92717c756cb711877f
Atsushi Watanabe <atsushi.w@ieee.org>
2024-04-17 16:43:20 +0900

    Debugging

--- test/src/e2e.cpp
+++ test/src/e2e.cpp
@@ -269,7 +269,10 @@ TEST_P(E2EWithParam, Simple)
     const int index = std::lround(scans_[i]->ranges[0] * 1000);
     ASSERT_NE(true_stamps_.find(index), true_stamps_.end()) << "Can not find corresponding ground truth timestamp";
     const double err = (true_stamps_[index] - scans_[i]->header.stamp).toSec();
-    EXPECT_LT(std::abs(err), 0.001) << "scan " << i << " gain " << status_msg_->sensor_clock_gain;
+    EXPECT_LT(std::abs(err), 0.001)
+        << std::setprecision(9) << std::fixed
+        << "scan " << i << " gain " << status_msg_->sensor_clock_gain
+        << " stamp " << scans_[i]->header.stamp;
     err_rms += err * err;
   }
   err_rms = std::sqrt(err_rms / 50);
